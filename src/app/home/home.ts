a0f8b1203fd79ed0730ae9023cbefd71b0ef881b
Onwordi Cyber <onwordi@reventtechnologies.com>
2026-04-28 19:46:15 +0100

    commented out subscription

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -9,58 +9,57 @@ import { Observable } from 'rxjs';
 import { State } from '../services/state/state';
 import { map } from 'rxjs/operators';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, Navbar, ProductList, RouterModule],
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  styleUrls: ['./home.css'],
 })
-
 export class Home implements OnInit {
   protected readonly title = signal('fashion-store');
 
-  @Input() data: IProductList[] = []
+  @Input() data: IProductList[] = [];
 
   products$!: Observable<IProductList[]>;
   loading$!: Observable<boolean>;
   error$!: Observable<string | null>;
   cartCount$!: Observable<number>;
 
-
-  constructor(private router: Router, private productService: ProductService, private state: State) { }
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private state: State,
+  ) {}
 
   ngOnInit(): void {
-
     this.cartCount$ = this.state.cartCount$;
     this.products$ = this.state.products$;
     this.loading$ = this.state.loading$;
     this.error$ = this.state.error$;
 
     this.state.setLoading(true);
-    this.productService.getAllProducts().subscribe({
-      next: products => {
-        this.state.setProducts(products);
-        this.state.setLoading(false);
-      },
-      error: err => {
-        this.state.setError(err);
-        this.state.setLoading(false);
-      }
-    });
+    // this.productService.getAllProducts().subscribe({
+    //   next: products => {
+    //     this.state.setProducts(products);
+    //     this.state.setLoading(false);
+    //   },
+    //   error: err => {
+    //     this.state.setError(err);
+    //     this.state.setLoading(false);
+    //   }
+    // });
   }
 
-  
   onProductSelected(product: IProductList): void {
-    this.state.addToCart(product); 
+    this.state.addToCart(product);
   }
 
   onSearchApp(query: string): void {
     if (query.length > 0) {
       const lowerQuery = query.toLowerCase();
       this.products$ = this.state.products$.pipe(
-        map(products => products.filter(p => p.name.toLowerCase().includes(lowerQuery)))
+        map((products) => products.filter((p) => p.name.toLowerCase().includes(lowerQuery))),
       );
     } else {
       this.products$ = this.state.products$;
